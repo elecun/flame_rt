beb52b4b27df43df19667695397a32abc24bfbea
elecun <elecun2@gmail.com>
2024-04-19 12:00:22 +0900

    modifying

---
+++ examples/01_lifecycle/lifecycle.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file lifecycle.cpp
+ * @author Byunghun Hwang (bh.hwang@iae.re.kr)
+ * @brief Life Cycle Task Example
+ * @version 0.1
+ * @date 2024-04-19
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+/* Warning */
+/*
+This example is incompleted example code.
+*/
+
+#include <flame/flame.hpp>
+
+using namespace flame;
+
+namespace example {
+
+    class lifecycle final : public TaskImpl<Periodic> {
+        public:
+            
+    }
+
+}
