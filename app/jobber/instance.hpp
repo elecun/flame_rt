d08579ec4a2f63808f1ee3b6d9955c3d22f8796e
elecun <elecun2@gmail.com>
2024-04-29 13:44:58 +0900

    added

---
+++ app/jobber/instance.hpp
@@ -0,0 +1,20 @@
+/**
+ * @file instance.hpp
+ * @author Byunghun Hwang<bh.hwang@iae.re.kr>
+ * @brief Flame Service Componet Management
+ * @version 0.1
+ * @date 2024-04-29
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+
+ #ifndef FLAME_APP_JOBBER_INCLUDED
+ #define FLAME_APP_JOBBER_INCLUDED
+
+ namespace flame {
+
+ } /* namespace */
+
+ #endif
