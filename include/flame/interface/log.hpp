f3925087a37c2c0d6ea409246ab75b8464a578af
elecun <elecun2@gmail.com>
2024-04-30 13:49:33 +0900

    fixed

--- include/flame/interface/log.hpp
+++ include/flame/interface/log.hpp
@@ -9,14 +9,18 @@
  * 
  */
 
-#ifndef FLAME_INTERFACE_LOG_HPP_INCLUDED
-#define FLAME_INTERFACE_LOG_HPP_INCLUDED
+#ifndef FLAME_RT_INTERFACE_LOG_HPP_INCLUDED
+#define FLAME_RT_INTERFACE_LOG_HPP_INCLUDED
 
+/* pre-defined & editable */
+#define _USE_SPDLOG_
+
+#ifdef _USE_SPDLOG_
 #include <dep/spdlog/spdlog.h>
 #include <dep/spdlog/sinks/stdout_color_sinks.h>
 #include <dep/spdlog/sinks/basic_file_sink.h>
-
 namespace console = spdlog;
+#endif
 
 
 #endif
